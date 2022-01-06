71a7da769fab5e964e73a1b907415af62dbe27da
yasintorun <yasintorun4680@gmail.com>
2022-01-06 21:24:19 +0300

    Created User Entity

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User : BaseEntity
+    {
+        public int UserId { get; set; }
+        public int Username { get; set; }
+        public int Email { get; set; }
+        public int Password { get; set; }
+        public int CreatedAt { get; set; }
+        public int Status { get; set; }
+    }
+}
