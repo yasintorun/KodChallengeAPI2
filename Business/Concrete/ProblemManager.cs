e17e8224e6802a66d9e8d81071d94185e12d321a
yasintorun <yasintorun4680@gmail.com>
2022-01-09 20:43:33 +0300

    Problem Entity eklendi

---
+++ Business/Concrete/ProblemManager.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Core.Business;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProblemManager : BaseManager<Problem>, IProblemService
+    {
+        private IProblemDal _problemDal;
+        public ProblemManager(IProblemDal problemDal) : base(problemDal)
+        {
+            _problemDal = problemDal;
+        }
+    }
+}
---
+++ Entities/Concrete/Problem.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Problem : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public int CreatedAt { get; set; }
+        public int CreatedByUserId { get; set; }
+
+        public int TrackId { get; set; }
+        public int DifficultyId { get; set; }
+    }
+}
