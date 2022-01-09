b59a8a0e1207d60ef4a00e0fb53e80bae5416c88
yasintorun <yasintorun4680@gmail.com>
2022-01-09 20:38:00 +0300

    Problemlerin zorluk dereceleri için Difficulty Entity oluşturuldu

---
+++ Business/Concrete/DifficultyManager.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Core.Business;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class DifficultyManager : BaseManager<Difficulty>, IDifficultyService
+    {
+        private IDifficultyDal _difficultyDal;
+        public DifficultyManager(IDifficultyDal difficultyDal) : base(difficultyDal)
+        {
+            _difficultyDal = difficultyDal;
+        }
+    }
+}
